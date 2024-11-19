6bed5599ba113df668efa3e1f92bb01608fef0b5
Valeria Bassani <valeria.bassani@vidiemme.it>
2024-11-19 10:36:10 +0100

    test commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,8 @@
-import { ApplicationConfig, inject } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-
-import { HttpLink } from 'apollo-angular/http';
-import { APOLLO_NAMED_OPTIONS, APOLLO_OPTIONS, provideApollo } from 'apollo-angular';
-import { InMemoryCache } from '@apollo/client/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -24,50 +24,36 @@ export class AuthComponent {
   error:string|null=null
   //constructor(private router: Router,private authService: AuthService) {}
 
-  // onSubmit(form:NgForm){
-  //   this.apollo
-  //   .watchQuery({
-  //     query: gql`
-  //       {
-  //         allPeople(first: 4) {
-  //           edges {
-  //             node {
-  //               name
-  //               birthYear
-  //               id
-  //             }
-  //           }
-  //         }
-  //       }
-  //     `,
-  //   })
-  //   .valueChanges.subscribe((result: any) => {
-  //     const edges = result?.data?.allPeople?.edges || [];
-  //     this.dataUsers = edges.map((edge: any) => ({
-  //       id: edge.node.id, // Converte l'ID in numero
-  //       name: edge.node.name,
-  //       bithDate: edge.node.birthYear,
-  //       tasks: [],
-  //     }));
-  //     console.log(this.dataUsers);
-  //   });
-  //   const { name, birthDate } = form.value;
-  //   const user = this.dataUsers.find(u => u.name === name && u.birthDate === birthDate);
-  //   console.log(user);
-  //   if(user){
-  //     this.error=null
-  //     this.authService.setUser(user);
-  //     this.router.navigate(['/task',"Home"]);
-  //   }else{
-  //     this.error="Utente non trovato"
-  //   }
-  //   form.reset();
-  // }
-
   onSubmit(form:NgForm){
-    this.users = JSON.parse(localStorage.getItem('users') || '[]') as User[];
-    const { name, lastname } = form.value;
-    const user = this.users.find(u => u.name === name && u.lastname === lastname);
+    this.apollo
+    .watchQuery({
+      query: gql`
+        {
+          allPeople(first: 4) {
+            edges {
+              node {
+                name
+                birthYear
+                id
+              }
+            }
+          }
+        }
+      `,
+    })
+    .valueChanges.subscribe((result: any) => {
+      const edges = result?.data?.allPeople?.edges || [];
+      this.dataUsers = edges.map((edge: any) => ({
+        id: edge.node.id, // Converte l'ID in numero
+        name: edge.node.name,
+        bithDate: edge.node.birthYear,
+        tasks: [],
+      }));
+      console.log(this.dataUsers);
+    });
+    const { name, birthDate } = form.value;
+    const user = this.dataUsers.find(u => u.name === name && u.birthDate === birthDate);
+    console.log(user);
     if(user){
       this.error=null
       this.authService.setUser(user);
@@ -77,5 +63,19 @@ export class AuthComponent {
     }
     form.reset();
   }
+
+  // onSubmit(form:NgForm){
+  //   this.users = JSON.parse(localStorage.getItem('users') || '[]') as User[];
+  //   const { name, lastname } = form.value;
+  //   const user = this.users.find(u => u.name === name && u.lastname === lastname);
+  //   if(user){
+  //     this.error=null
+  //     this.authService.setUser(user);
+  //     this.router.navigate(['/task',"Home"]);
+  //   }else{
+  //     this.error="Utente non trovato"
+  //   }
+  //   form.reset();
+  // }
 
 }
